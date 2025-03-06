4a7a2bcf97d817b746e6dbd9e64d46ccdbe90ff7
Oblytoid <Cauan.nogueira@outlook.com.br>
2025-03-06 15:36:26 -0300

    Conclusão do desafio DIO.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Iphone iphone = new Iphone();
+
+        iphone.reproduzirMusica("Teste");
+        iphone.selecionarMusica("teste2");
+        iphone.pararMusica();
+
+        iphone.Atender();
+        iphone.Ligar("+55 12 32134 1423");
+        iphone.Desligar();
+        iphone.iniciarCorreioVoz();
+
+        iphone.adicionarPagina();
+        iphone.atualizarPagina();
+        iphone.exibirPagina();
+        iphone.removerPagina();
     }
 }
